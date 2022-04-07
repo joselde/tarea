c907660e9452af98eb0f30b6c12339ec8b7a37f9
joselde <103205076+joselde@users.noreply.github.com>
2022-04-07 12:05:43 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="alert alert-info" role="alert">
+        <h2>HOLA MUNDO, <?php echo date('H:m:s')?></h2>
+    </div>
+    
+</body>
+</html>
